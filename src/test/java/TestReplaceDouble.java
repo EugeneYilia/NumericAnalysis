7b6e238c82a550622252ff48008078a9a53ab00d
EugeneLiu <38400679+EugeneYilia@users.noreply.github.com>
2018-05-18 03:00:28 +0800

    Delete TestReplaceDouble.java

--- src/test/java/TestReplaceDouble.java
+++
@@ -1,7 +0,0 @@
-public class TestReplaceDouble {
-    public static void main(String[] args) {
-        String x1 = "x+1+8+x";
-        System.out.println(x1.replaceAll("x", String.valueOf(3.3)));
-        System.out.println(Double.parseDouble(x1.replaceAll("x", String.valueOf(3.3))));
-    }
-}
